e918f297bf680f528c5e8cffc902ca90dc63481d
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-05-25 13:36:53 -0300

    api routes finished

--- api/task.js
+++ api/task.js
@@ -39,4 +39,30 @@ module.exports = app => {
             })
             .catch(err => res.status(400).json(err))
     }
+
+    const updateTaskDoneAt = (req, res, done) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .update({ doneAt })
+            .then(_ => res.status(204).send())
+            .catch(err =>  res.status(400).json(err))
+    }
+
+    const toggleTask = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .first()
+            .then(task => {
+                if (!task) {
+                    const msg = `Task com id ${req.params.id} não encontrada.`
+                    return res.status(400).send(msg)
+                }
+
+                const done = task.done ? null : new Date()
+                updateTaskDoneAt(req, res, done)
+            })
+            .catch(err => res.status(400).json(err))
+    }
+
+    return { getTasks, save, remove, toggleTask }
 }
