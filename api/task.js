da2a1c25223663c91c17e497748570b172a7c618
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-05-25 08:54:08 -0300

    api verbs implementation

---
+++ api/task.js
@@ -0,0 +1,42 @@
+const moment = require('moment')
+
+module.exports = app => {
+    const getTasks = (req, res) => {
+        const date = req.query.date ? req.query.date : moment().endOf('day').toDate()
+
+        app.db('tasks')
+            .where({ userId: req.user.id })
+            .where('estimate', '<=', date)
+            .orderBy('estimate')
+            .then(tasks => res.json(tasks))
+            .catch(err => res.status(400).json(err))
+    }
+
+    const save = (req, res) => {
+        if (!req.body.desc.trim()) {
+            return res.status(400).send('A descrição da tarefa é obrigatória')
+        }
+
+        req.body.userId = req.user.id
+
+        app.db('tasks')
+            .insert(req.body)
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(400).json(err))
+    }
+
+    const remove = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .del()
+            .then(rowsDeleted => {
+                if(rowsDeleted > 0) {
+                    res.status(204).send()
+                } else {
+                    const msg = `Id:${req.params.id} não encontrado para task.`
+                    res.status(400).send(msg)
+                }
+            })
+            .catch(err => res.status(400).json(err))
+    }
+}
